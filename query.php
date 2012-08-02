89535e81728689a14d39ca4c4ae65dde257264ba
Matthew Cranham <cranman@hotmail.co.uk>
2012-08-02 16:45:15 +0100

    initial commit getting stats from kf statsfeed

---
+++ query.php
@@ -0,0 +1,42 @@
+<?php
+
+	if(isset($_GET['name'])) {
+		$name = $_GET['name'];
+
+		$id = processName($name);
+
+		$stats = getKFStatsfeed($id);
+
+		generateJSON($stats);
+	}
+
+	function processName($name) {
+		if (preg_match('/[0-9]{17}/', $name)) {
+			return 'profiles/'.$name;
+		} else {
+			return 'id/'.$name;
+		}
+	}
+
+	function getKFStatsfeed($id) {
+		$HEAD_URL = 'http://steamcommunity.com/';
+		$TAIL_URL = '/statsfeed/1250';
+
+		$url = $HEAD_URL.$id.$TAIL_URL;
+
+		$xml = simplexml_load_file($url) or die ('Error loading XML data');
+
+		foreach($xml->stats->item as $item) {
+			$stats[(string) $item->APIName] = (string) $item->value;
+		}
+
+		return $stats;
+	}
+
+	function generateJSON($stats) {
+		header('content-type: application/json');
+
+		$json = json_encode($stats);
+
+		echo($json);
+	}
