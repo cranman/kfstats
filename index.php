575dc6ba890bc9f1141ff6edc041f46a414f2699
Matthew Cranham <cranman@hotmail.co.uk>
2012-08-02 16:50:22 +0100

    started to structure the page

--- index.php
+++ index.php
@@ -2,5 +2,12 @@
 	<head>
 	</head>
 	<body>
+		<div id="container">
+			<div id="header"></div>
+			<div id="page">
+				<div id="stats"></div>
+			</div>
+			<div id="footer"></div>
+		</div>
 	</body>
 </html>
