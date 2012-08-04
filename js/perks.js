ebed9da2bdd26c845791fddb0183912d79315a3e
Matthew Cranham <cranman@hotmail.co.uk>
2012-08-04 17:59:16 +0100

    got zerk, fb, demo perks to work

--- js/perks.js
+++ js/perks.js
@@ -3,9 +3,9 @@ calcPerkLevels = function(data) {
 //	support(data.weldingpoints, data.shotgundamage);
 	sharp(data.headshotkills);
 //	commando(data.stalkerkills, data.bullpupdamage);
-//	berzerker(data.meleedamage);
-//	firebug(data.flamethrowerdamage);
-//	demo(data.explosivesdamage);
+	damageperk("berzerker", data.meleedamage);
+	damageperk("firebug", data.flamethrowerdamage);
+	damageperk("demo", data.explosivesdamage);
 }
 
 medic = function(heal) {
@@ -92,4 +92,47 @@ sharp = function(headshots) {
 
         $('#sharp-level').html('<h4>Level: ' + level + '</h4>');
         $('#sharp-progress').html('<h4>Progress: ' + headshots + ' / ' + progress +' Headshots</h4>');
+}
+
+damageperk = function(perk, damage) {
+	var val;
+	var level;
+	var progress;
+
+	if (damage < 25000) {
+                val = damage / 250;
+                level = 0;
+                progress = 25000;
+	} else if (damage < 100000) {
+                val = (damage - 25000) / 750;
+                level = 1;
+                progress = 100000;
+	} else if (damage < 500000) {
+                val = (damage - 100000) / 4000;
+                level = 2;
+                progress = 500000;
+	} else if (damage < 1500000) {
+                val = (damage - 500000) / 10000;
+                level = 3;
+                progress = 1500000;
+	} else if (damage < 3500000) {
+                val = (damage - 1500000) / 20000;
+                level = 4;
+                progress = 3500000;
+	} else if (damage < 5500000) {
+                val = (damage - 3500000) / 20000;
+                level = 5;
+                progress = 5500000;
+	} else {
+                val = Math.min((damage - 5500000) / 55000, 100);
+                level = 6;
+                progress = 11000000;
+	}
+
+        $('#'+perk+'-bar').progressbar({
+                value: val
+        });
+
+        $('#'+perk+'-level').html('<h4>Level: ' + level + '</h4>');
+        $('#'+perk+'-progress').html('<h4>Progress: ' + damage + ' / ' + progress +' Damage Points Required</h4>');
 }
