f027635ecdb19875459046e9891ee3e646fb2ad9
Matthew Cranham <cranman@hotmail.co.uk>
2012-08-09 23:42:37 +0100

    added some basic css

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 	<head>
 		<link type="text/css" href="css/smoothness/jquery-ui-1.8.22.custom.css" rel="stylesheet" />
+		<link type="text/css" href="css/kfstats.css" rel="stylesheet" />
 
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 		<script type="text/javascript" src="js/jquery-ui-1.8.22.custom.min.js"></script>
