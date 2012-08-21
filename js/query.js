0af04674abd15a776a354211eab4a8dfd8ab2b9a
Matthew Cranham <cranman@hotmail.co.uk>
2012-08-21 17:07:49 +0100

    splits between map achievements and other achievements

--- js/query.js
+++ js/query.js
@@ -19,18 +19,38 @@ getStatsFeed = function(name) {
 getAchievements = function(name) {
         $('#achievements').html('<h2>Achievements</h2>');
         $('#errors').text('');
+
+	var maps = ['bioticslab', 'westlondon', 'departed', 'icecave', 'manor', 'farm', 'offices', 'foundry', 'asylum', 'wyre', 'biohazard', 'crash', 'filthscross', 'hospitalhorrors', 'icebreaker', 'mountainpass', 'suburbia', 'waterworks', 'santasevillair', 'sideshow', 'hellride'];
+	var difficulties = ['normal', 'hard', 'suicidal', 'hell'];
+
+	// Generates the map and difficulty name for each map achievement
+	var achievements = [];
+	maps.forEach(function(map) { difficulties.forEach(function(diff) { achievements.push('win' + map + diff); }); });
+
+	// The aperture achievements are in a different form
+	var aperture = ['achievement133', 'achievement134', 'achievement135', 'achievement136'];
+	achievements = achievements.concat(aperture);
+
         var query = 'query.php?name=' + name + '&type=achievements';
         var jqxhr = $.getJSON(query, function(data) {
                 $.each(data, function(key, val) {
-                        $('#achievements').append('<div id='+key+' class="achievement"></div>');
+			if (isMapAchievement(achievements, key) == -1) {
+	                        $('#achievements').append('<div id='+key+' class="achievement"></div>');
+			} else {
+				$('#maps').append('<div id='+key+' class="achievement"></div>');
+			}
 
-			$('#'+key).append('<img src="'+val[1]+'">');
-			$('#'+key).append('<div class="achievementname">'+val[0]);
-			$('#'+key).append('<div class="achievementtext">'+val[2]+'<br>Unlocked at: '+ (new Date(val[3] * 1000)).toDateString()+'</div>');
+                        $('#'+key).append('<img src="'+val[1]+'">');
+                        $('#'+key).append('<div class="achievementname">'+val[0]);
+                        $('#'+key).append('<div class="achievementtext">'+val[2]+'<br>Unlocked at: '+ (new Date(val[3] * 1000)).toDateString()+'</div>');
                 });
         }).error(function(jqXHR, textStatus, errorThrown) {
                 $('#errors').text(jqXHR.responseText);
         });
 
         return false;
+}
+
+isMapAchievement = function(achievements, name) {
+	return achievements.indexOf(name);
 }
